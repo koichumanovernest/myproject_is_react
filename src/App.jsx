c3ac1b6860e28a8708db805f004e3f52f662619f
koichumanovernest <koichumanovernest@gmail.com>
2024-01-24 19:40:37 +0600

    added  my_project

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import ResultRender from "./Components/Result/ResultRender";
+
+function App() {
+	const [data, setData] = useState([]);
+
+	
+	const getData = async () => {
+		const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+		const responseData = await response.json();
+		console.log(responseData);
+		setData(responseData);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div className="App">
+			<ResultRender />
+			<div className="button">
+				<button
+					onClick={() => {
+						setData([]);
+					}}>
+					delete All
+				</button>
+				<button
+					onClick={() => {
+						getData()
+					}}>
+					add All
+				</button>
+			</div>
+			<div className="divAll">
+				{data.map((item, index) => (
+					<div className="allDiv" key={item.id}>
+						<p>{item.id}</p>
+						<h1>{item.title}</h1>
+						<p>{item.body}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Result/ResultRender.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './Result.css'
+import { useState } from "react";
+import { useEffect } from "react";
+
+ const ResultRender = () => {
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  useEffect(() => {
+    console.log(inputValue1.length);
+  }, [inputValue1]);
+  const handleChange = (event) => {
+    setInputValue1(event.target.value);
+  };
+  const resultValue = (text) => {
+    localStorage.setItem("key", inputValue);
+    setInputValue(text);
+  };
+  useEffect(() => {
+    resultValue();
+  }, []);
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={inputValue1}
+        onChange={handleChange}
+        placeholder=" console.log co sandar tyshot"
+      />
+      <input
+        type="text"
+        onChange={(e) => {
+          resultValue(e.target.value);
+        }}
+        placeholder=" localStorage jazilat"
+      />
+    </div>
+  );
+};
+
+export default ResultRender
